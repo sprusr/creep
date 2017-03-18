cac969e5d78f07eec9d55db7c6ef9aa62038056b
sprusr <scott@spru.sr>
2017-03-18 15:07:29 +0000

    Implement facebook login

--- server.py
+++ server.py
@@ -1,13 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request, json, render_template
+from facebook import GraphAPI
+import os
+
+FB_APP_ID = os.environ.get("FB_APP_ID")
+FB_APP_NAME = os.environ.get("FB_APP_NAME")
+FB_APP_SECRET = os.environ.get("FB_APP_SECRET")
+
 app = Flask(__name__)
 
-@app.route("/webhook", methods=['GET'])
-def webhook_get():
-    return request.args.get("hub.challenge", "")
+@app.route("/auth", methods=["GET"])
+def auth_post():
+    return "HELP ME " + request.args.get("id")
 
+@app.route("/webhook", methods=["GET"])
+def webhook_get():
+    return request.args.get("hub.challenge")
+
+@app.route("/webhook", methods=["POST"])
 def webhook_post():
-    # do something
+    data = request.get_json()
+    print(json.dumps(data))
     return "todo"
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
